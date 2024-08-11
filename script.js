71e629162d950da09a0b59b5faeaadd2e8618b83
unknown <nanditakumari311@gmail.com>
2024-08-12 00:19:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,122 @@
+
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    delay:500,
+    interval:500
+  };
+
+ScrollReveal().reveal(".name h1", {
+    ...scrollRevealOption,
+    origin:"bottom",
+    distance:"50px",
+    duration:2000
+  })
+
+  ScrollReveal().reveal(".name p", {
+    ...scrollRevealOption,
+    origin:"bottom",
+    opacity:0,
+    distance:"50px",
+    duration:2000,
+    delay:1000,
+  });
+
+  ScrollReveal().reveal(".heroimage .quote", {
+    ...scrollRevealOption,
+    origin:"left",
+    distance:"100px",
+    duration:2000,
+    delay:1500,
+  });
+
+  ScrollReveal().reveal(".image img", {
+    ...scrollRevealOption,
+    origin:"right",
+    distance:"100px",
+    duration:2000,
+    delay:2000,
+  });
+
+  ScrollReveal().reveal(".image h1", {
+    ...scrollRevealOption,
+    origin:"right",
+    distance:"100px",
+    duration:2000,
+    delay:2500,
+  });
+
+  ScrollReveal().reveal(".heading h1", {
+    ...scrollRevealOption,
+    origin:"top",
+    distance:"100px",
+    delay:1000,
+    duration:2000,
+  });
+
+  
+
+  ScrollReveal().reveal(".time-desc h1", {
+    ...scrollRevealOption,
+    origin:"left",
+    distance:"100px",
+    duration:1000,
+  });
+
+  ScrollReveal().reveal(".time-desc p", {
+    ...scrollRevealOption,
+    origin:"bottom",
+    distance:"100px",
+    duration:1000,
+  });
+
+  ScrollReveal().reveal(".qualities h1", {
+    ...scrollRevealOption,
+    origin:"top",
+    distance:"100px",
+    duration:2000,
+  });
+
+
+  ScrollReveal().reveal(".qaualityinfoleft", {
+    ...scrollRevealOption,
+    origin:"left",
+    distance:"100px",
+    duration:1000,
+  });
+
+  ScrollReveal().reveal(".qaualityinforight", {
+    ...scrollRevealOption,
+    origin:"right",
+    distance:"100px",
+    duration:1000,
+
+  });
+
+  ScrollReveal().reveal(".awards h1", {
+    ...scrollRevealOption,
+    origin:"top",
+    distance:"100px",
+    duration:1000,
+
+  });
+
+  ScrollReveal().reveal(".awardinfo", {
+    ...scrollRevealOption,
+    origin:"bottom",
+    distance:"100px",
+    duration:1000,
+
+  });
+
+  
+  ScrollReveal().reveal(".knowmore", {
+    ...scrollRevealOption,
+    distance:"100px",
+    duration:1000,
+    opacity:0,
+
+  });
+
+  
